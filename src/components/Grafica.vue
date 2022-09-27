cdfdf6240b7303a63d24ed4b96f11334e4446951
JoseArevaloz7 <arevaloz_j@outlook.com>
2022-09-26 20:54:33 -0500

    .

--- src/components/Grafica.vue
+++ src/components/Grafica.vue
@@ -1,13 +1,57 @@
 <template>
+    <div id="iniG">
+      <canvas id="myChart"></canvas>
+    </div>
+  </template>
   
-</template>
-
-
-<script>
-
-</script>
-
-
-<style>
-
-</style>
+  <script>
+  
+  import Chart from 'chart.js/auto';
+  
+    export default {
+    mounted(){
+      const ctx = document.getElementById('myChart');
+      const myChart = new Chart(ctx, {
+      
+      type: 'bar',
+      data: {
+          labels: ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado'],
+          datasets: [{
+              label: '# of Votes',
+              data: [12, 19, 3, 5, 2, 10, 1],
+              backgroundColor: [
+                  'rgba(0, 173, 181,  0.2)',
+                  'rgba(0, 0, 0, 0.2)',
+                  'rgba(196, 251, 107, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(0, 173, 181, 1)',
+                  'rgba(0, 0, 0, 1)',
+                  'rgba(196, 251, 107, 1)'
+              ],
+              borderWidth: 1
+          }]
+      },
+      options: {
+          scales: {
+              y: {
+                  beginAtZero: true
+              }
+          }
+      }
+  });
+  myChart;
+    }
+  }
+  </script>
+  
+  <style scoped>
+  canvas{
+    width: 100px;
+    height: 100px;
+  }
+  #iniG{
+    width: 400px;
+    height: 200px;
+  }
+  </style>
--- src/views/Alimento.vue
+++ src/views/Alimento.vue
@@ -1,13 +1,23 @@
 <template>
   <div class="alimento">
+    {{casetas}}
     <h1>Alimento</h1>
+    <Grafica/>
   </div>  
 </template>
 
 <script>
-export default {
+  import Grafica from "../components/Grafica.vue"
+  import {mapActions, mapState} from 'vuex'
 
-}
+  export default {
+    components:{
+      Grafica,
+    },
+    computed:{
+      ...mapState(['casetas'])
+    }
+  }
 </script>
 
 <style scoped>
