97166acf7488d710e0fc10bac692b218145254d7
JoseArevaloz7 <arevaloz_j@outlook.com>
2022-10-07 17:36:42 -0500

    grafica finish

--- src/components/Grafica.vue
+++ src/components/Grafica.vue
@@ -1,9 +1,9 @@
 <template>
+  {{option}}
     <div id="iniG">
-      
       <canvas id="myChart"  width="400" height="400"></canvas>
       <!-- <canvas id="myChart" ></canvas> -->
-      <button v-on:click="this.grafica"> press</button>
+      <button v-on:click="this.show_gra"> press</button>
     </div>
   </template>
   
@@ -17,11 +17,14 @@ import Chart from 'chart.js/auto';
         return{
             totalSeman: [0,15,10,4,3,3,1],
             banGra: true,
-            myChart:[]
+            myChart:[],
+            aux:"",
+            ban: true,
           }
       },
       props: {
-        totalHuevos: Object
+        totalHuevos: Object,
+        option:String
       },
       // mounted(){
       //   this.grafica()
@@ -29,86 +32,61 @@ import Chart from 'chart.js/auto';
       
       methods: {
         
-        grafica(){
-          if(this.banGra){
-            console.log(1)
-            this.banGra = false
+        show_gra(){
+          if(this.aux != this.option){
+            this.ban = true
+          }
+        
+          if(this.ban){
+            this.ban = false
+            this.aux = this.option
+            console.log(this.aux)
 
+            if(this.banGra){
+              this.banGra = false
+              this.grafica()
+            }
+
+            else{
+              console.log(3)
+              this.myChart.destroy()
+              this.grafica()
+            }
+          }
+        },
+        grafica(){
           const ctx = document.getElementById('myChart');
           
           // myChart.refresh
           this.myChart = new Chart(ctx, {
             type: 'bar',
             data: {
-                labels: ['Sabado', 'Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes'], 
-                datasets: [{
-                    label: '# of Votes',
-                    data: this.totalHuevos,
-                    backgroundColor: [
-                        'rgba(0, 173, 181,  0.2)',
-                        'rgba(0, 0, 0, 0.2)',
-                        'rgba(196, 251, 107, 0.2)'
-                    ],
-                    borderColor: [
-                        'rgba(0, 173, 181, 1)',
-                        'rgba(0, 0, 0, 1)',
-                        'rgba(196, 251, 107, 1)'
-                    ],
-                    borderWidth: 1,
-                }]
+              labels: ['Sabado', 'Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes'], 
+              datasets: [{
+                label: '# of Votes',
+                data: this.totalHuevos,
+                backgroundColor: [
+                  'rgba(0, 173, 181,  0.2)',
+                  'rgba(0, 0, 0, 0.2)',
+                  'rgba(196, 251, 107, 0.2)'
+                ],
+                borderColor: [
+                  'rgba(0, 173, 181, 1)',
+                  'rgba(0, 0, 0, 1)',
+                  'rgba(196, 251, 107, 1)'
+                ],
+                borderWidth: 1,
+              }]
             },
             options: {
-                scales: {
-                  y: {
-                        beginAtZero: true
-                    }
+              scales: {
+                y: {
+                  beginAtZero: true
                 }
+              }
             }
           });
-         
           this.myChart;
-          }
-          else{
-            console.log(3)
-            // this.myChart.data.datasets[0].data = this.totalSemana; // Would update the first dataset's value of 'March' to be 50
-            // this.myChart.update();
-            // this.banGra = true
-          this.myChart.destroy()
-          const ctx = document.getElementById('myChart');
-          
-          // myChart.refresh
-          this.myChart = new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: ['Sabado', 'Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes'], 
-                datasets: [{
-                    label: '# of Votes',
-                    data: this.totalHuevos,
-                    backgroundColor: [
-                        'rgba(0, 173, 181,  0.2)',
-                        'rgba(0, 0, 0, 0.2)',
-                        'rgba(196, 251, 107, 0.2)'
-                    ],
-                    borderColor: [
-                        'rgba(0, 173, 181, 1)',
-                        'rgba(0, 0, 0, 1)',
-                        'rgba(196, 251, 107, 1)'
-                    ],
-                    borderWidth: 1,
-                }]
-            },
-            options: {
-                scales: {
-                  y: {
-                        beginAtZero: true
-                    }
-                }
-            }
-          });
-         
-          this.myChart;
-          
-          }
         }
       },
   }
